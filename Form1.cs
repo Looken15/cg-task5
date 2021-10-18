1b54cf0e620cbb7be9d946cfad1a2b08480eb508
Looken15 <tema-super-man@yandex.ru>
2021-10-18 22:22:14 +0300

    L-systems

---
+++ Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace task5
+{
+    public partial class Form1 : Form
+    {
+        L_System system;
+        int generation = 2;
+        Graphics g;
+        Color line_color = Color.Black;
+        int from_x;
+        int from_y;
+
+        string long_rule;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            system = new L_System("l-system.txt");
+            richTextBox1.Text = File.ReadAllText("l-system.txt");
+
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = Graphics.FromImage(pictureBox1.Image);
+
+            var s = system.ApplyRules(generation);
+            richTextBox1.Text += "\n\n" + s;
+
+            textBox1.Text = generation.ToString();
+        }
+
+        public void Interpret(float x, float y)
+        {
+            double len = 100.0 / Math.Pow(2, generation);
+            var angle = DirToAngle(system.dir) * Math.PI / 180.0;
+
+            for (int i = 0; i < long_rule.Length; ++i)
+            {
+                if (new char[] { 'F', 'A', 'B', 'X', 'Y' }.Contains(long_rule[i]))
+                {
+                    (x, y) = DrawLine(x, y, angle, len);
+                }
+                else if (long_rule[i] == '-')
+                {
+                    angle -= system.angle;
+                }
+                else if (long_rule[i] == '+')
+                {
+                    angle += system.angle;
+                }
+                else if (long_rule[i] == '[')
+                {
+                    ++i;
+                    InterpretStep(ref i, x, y, angle, len);
+                }
+            }
+        }
+
+        private void InterpretStep(ref int i, float x, float y, double angle, double len)
+        {
+            while (long_rule[i] != ']')
+            {
+                if (new char[] { 'F', 'A', 'B', 'X', 'Y' }.Contains(long_rule[i]))
+                {
+                    (x, y) = DrawLine(x, y, angle, len);
+                }
+                else if (long_rule[i] == '-')
+                {
+                    angle -= system.angle;
+                }
+                else if (long_rule[i] == '+')
+                {
+                    angle += system.angle;
+                }
+                else if (long_rule[i] == '[')
+                {
+                    ++i;
+                    InterpretStep(ref i, x, y, angle, len);
+                }
+                ++i;
+            }
+        }
+
+        double DirToAngle(dir dir)
+        {
+            switch (dir)
+            {
+                case dir.up: return 270.0;
+                case dir.right: return 0.0;
+                case dir.down: return 90.0;
+                case dir.left: return 180.0;
+                default: return 0.0;
+            };
+        }
+
+        (float, float) DrawLine(float x, float y, double angle, double len)
+        {
+            float new_x = (float)(x + Math.Cos(angle) * len);
+            float new_y = (float)(y + Math.Sin(angle) * len);
+
+            g.DrawLine(new Pen(line_color, 2), x, y, new_x, new_y);
+            pictureBox1.Refresh();
+
+            return (new_x, new_y);
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            g.Clear(Color.White);
+            var me = (MouseEventArgs)e;
+            long_rule = system.ApplyRules(generation);
+            Interpret(me.X, me.Y);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            int.TryParse(textBox1.Text, out generation);
+        }
+    }
+}
---
+++ L-System.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace task5
+{
+    public enum dir { up, down, left, right };
+
+    public class L_System
+    {
+        public String init;
+        public Dictionary<char, String> rules = new Dictionary<char, string>();
+        public dir dir;
+        public double angle;
+
+        public L_System(String filename)
+        {
+            try
+            {
+                var lines = File.ReadAllLines(filename).Select(s => s.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToArray());
+                init = lines.First()[0];
+                angle = double.Parse(lines.First()[1]) * Math.PI / 180.0;
+                dir = (dir)Enum.Parse(typeof(dir), lines.First()[2]);
+
+                foreach (var line in lines.Skip(1))
+                {
+                    if (line[1] == "_")
+                        rules.Add(line[0][0], "");
+                    else
+                        rules.Add(line[0][0], line[1]);
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        public String ApplyRules(int generation)
+        {
+            String res = init;
+
+            for (int i = 0; i < generation; ++i)
+            {
+                string new_str = "";
+                for (int j = 0; j < res.Length; ++j)
+                {
+                    if (rules.ContainsKey(res[j]))
+                    {
+                        new_str += rules[res[j]];
+                    }
+                    else
+                    {
+                        new_str += res[j];
+                    }
+                }
+                res = new_str;
+            }
+            return res;
+        }
+    }
+}
